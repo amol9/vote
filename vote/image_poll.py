e8e3787862eaa569b732ae79488ec86eb26e2db2
Amol <babaiscool@gmail.com>
2017-03-19 20:50:40 +0530

    Added reddit_image_poll2 to support poll with multiple images. Added multiple image support to ImagePoll.

--- vote/image_poll.py
+++ vote/image_poll.py
@@ -8,81 +8,95 @@ from redlib.api.system import sys_command, is_windows, is_linux
 from .straw_poll import StrawPoll
 
 
+class Image:
+
+	def __init__(self, title, url):
+	    self.title	= title
+	    self.url	= url
+
+
 class ImagePoll:
-        cache_folder = 'image_cache'
+	cache_folder = 'image_cache'
 
 
-        def __init__(self, title, image_url, poll_url, cache_images=True, cache_folder=None):
-                self._title     = title
-                self._image_url = image_url
-                self._poll_url  = poll_url
+	def __init__(self, title, poll_url, cache_images=True, cache_folder=None, images=None):
+		self._title	= title
+		self._poll_url	= poll_url
+		self._images	= images
 
-                self._cache_images = cache_images
-                self._cache_folder = cache_folder or self.cache_folder
+		self._cache_images = cache_images
+		self._cache_folder = cache_folder or self.cache_folder
 
-                self._strawpoll = StrawPoll()
-
-
-        def vote(self):
-                print(self._title)
-                poll_id = self._poll_url.split('/')[-1]
-
-                if self._strawpoll.already_voted(poll_id):
-                        print('already voted')
-                        return True
-                        
-                filepath = self._download_image()
-                self._show_image(filepath)
-
-                return self._strawpoll.vote(poll_id)
+		self._strawpoll = StrawPoll()
 
 
-        def _show_image(self, filepath):
-                if is_windows():
-                        sys_command("cmd /c start %s"%filepath)
-                elif is_linux():
-                        run(("xdg-open %s"%filepath).split())
-                else:
-                        print('unsupported OS')
-                
+	def vote(self):
+		print(self._title)
+		poll_id = self._poll_url.split('/')[-1]
 
-        def _download_image(self):
-                filename = self._image_url.split('/')[-1]
-                filepath = self._lookup_cache(filename)
-                if filepath is not None:
-                        return filepath
+		if self._strawpoll.already_voted(poll_id):
+			print('already voted')
+			return True
+			
+		if self._images is not None:
+			for image in self._images:
+				if image.title is not None:
+					print(image.title)
+				try:
+					filepath = self._download_image(image.url)
+					self._show_image(filepath)
+				except KeyboardInterrupt:
+					print('skipping..')	
 
-                print('getting image: %s'%self._image_url)
-                response = requests.get(self._image_url)
-
-                if not self._cache_images:
-                        return self._save_image(response.content, filename)
-                else:
-                        return self._cache_image(response.content, filename)
+		return self._strawpoll.vote(poll_id)
 
 
-        def _save_image(self, data, filename):
-                with open(filename, 'wb') as f:
-                        f.write(data)
+	def _show_image(self, filepath):
+		if is_windows():
+			sys_command("cmd /c start %s"%filepath)
+		elif is_linux():
+			run(("xdg-open %s"%filepath).split())
+		else:
+			print('unsupported OS')
+		
 
-                return filename
+	def _download_image(self, url):
+		filename = url.split('/')[-1]
+		filepath = self._lookup_cache(filename)
+		if filepath is not None:
+			return filepath
+
+		print('getting image: %s'%url)
+		response = requests.get(url)
+
+		if not self._cache_images:
+			return self._save_image(response.content, filename)
+		else:
+			return self._cache_image(response.content, filename)
 
 
-        def _cache_image(self, data, filename):
-                if not exists(self._cache_folder):
-                        mkdir(self._cache_folder)
+	def _save_image(self, data, filename):
+		with open(filename, 'wb') as f:
+			f.write(data)
 
-                filepath = joinpath(self.cache_folder, filename)
-
-                with open(filepath, 'wb') as f:
-                        f.write(data)
-
-                return filepath
+		return filename
 
 
-        def _lookup_cache(self, filename):
-                filepath = joinpath(self.cache_folder, filename)
-                if exists(filepath):
-                        return filepath
-                else:
-                        return None
+	def _cache_image(self, data, filename):
+		if not exists(self._cache_folder):
+			mkdir(self._cache_folder)
+
+		filepath = joinpath(self.cache_folder, filename)
+
+		with open(filepath, 'wb') as f:
+			f.write(data)
+
+		return filepath
+
+
+	def _lookup_cache(self, filename):
+		filepath = joinpath(self.cache_folder, filename)
+		if exists(filepath):
+			return filepath
+		else:
+			return None
--- vote/polls.py
+++ vote/polls.py
@@ -1,47 +1,80 @@
 import re
 
 from .reddit_post import RedditPost
-from .image_poll import ImagePoll
+from .image_poll import ImagePoll, Image
 from .straw_poll import StrawPollError, VotePass
 
 
+class PollError(Exception):
+    pass
+
+
 class Polls:
-        
-        def __init__(self):
-                self._map = {
-                        'reddit_image_poll'     : self._reddit_image_poll
-                }
+	
+	def __init__(self):
+		self._map = {
+			'reddit_image_poll'	: self._reddit_image_poll,
+			'reddit_image_poll2'	 : self._reddit_image_poll2
+		}
 
 
-        def run(self, poll_name, params=None):
-                if not poll_name in self._map.keys():
-                        print('no such poll')
-                        print('available polls:\n' + '\n'.join(self._map.keys()))
-                        return
+	def run(self, poll_name, params=None):
+		if not poll_name in self._map.keys():
+			print('no such poll')
+			print('available polls:\n' + '\n'.join(self._map.keys()))
+			return
 
-                method = self._map[poll_name]
-                method(params)
+		method = self._map[poll_name]
+		try:
+		    method(params)
+		except PollError as e:
+		    print(e)
 
 
-        def _reddit_image_poll(self, params):
-                if not 'post_id' in params.keys():
-                        print('please provide a reddit post id')
-                        return
+	def _reddit_image_poll(self, params):
+		j = self._get_reddit_post_content(params)
 
-                rp = RedditPost(post_id=params['post_id'], cache=True)
-                j = rp.content
+		list_item_regex = re.compile("\d+\.\s+\[(.*?)\]\((.*?)\).*\((.*?)\)")
+		matches = list_item_regex.findall(j)
 
-                list_item_regex = re.compile("\d+\.\s+\[(.*?)\]\((.*?)\).*\((.*?)\)")
-                matches = list_item_regex.findall(j)
+		for m in matches:
+			ip = ImagePoll(title=m[0].strip(), images=Image(None, m[1].strip()), poll_url=m[2].strip(), cache_images=True)
+			self._ip_vote()
+	    
 
-                for m in matches:
-                        ip = ImagePoll(title=m[0].strip(), image_url=m[1].strip(), poll_url=m[2].strip(), cache_images=True)
-                        try:
-                                success = ip.vote()
-                                if not success:
-                                        print('failure in casting vote')
-                        except VotePass:
-                                pass
-                        except StrawPollError as e:
-                                print(e)
-                                break
+	def _get_reddit_post_content(self, params):
+		if not 'post_id' in params.keys():
+			raise PollError('please provide a reddit post id')
+
+		rp = RedditPost(post_id=params['post_id'], cache=True)
+		return rp.content
+
+
+	def _ip_vote(self, ip):
+		try:
+			success = ip.vote()
+			if not success:
+				print('failure in casting vote')
+		except VotePass:
+			pass
+		except StrawPollError as e:
+			print(e)
+
+
+	def _reddit_image_poll2(self, params):
+		j = self._get_reddit_post_content(params)
+
+		list_item_regex = re.compile("^\d+\..*$", re.M)
+		matches = list_item_regex.findall(j)
+
+		link_regex = re.compile("\[(.*?)\]\((.*?)\)")
+		for m in matches:
+			links = link_regex.findall(m)
+			images = []
+			for l in links[:-1]:
+				img = Image(l[0].strip(), l[1].strip())
+				images.append(img)
+
+			ip = ImagePoll(title=links[0][0].strip(), images=images, poll_url=links[-1][1].strip(), cache_images=True)
+			self._ip_vote(ip)
+       
